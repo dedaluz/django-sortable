69a84830e782f7313c9c8d7610387eb6b31e33d8
Dominique Lederer <dominique.lederer@wunderweiss.com>
2012-03-27 17:04:16 +0200

    adaptions

--- sortable/admin.py
+++ sortable/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
-
 class SortableAdmin(admin.ModelAdmin):
-    # Make instances reorderable
-    list_editable = ('position',)
-    list_display = ('position', )
 
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js',
             'js/django-admin-sortable.js',)
--- sortable/models.py
+++ sortable/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
-
 class Sortable(models.Model):
-    # Make instances reorderable
-    position = models.IntegerField(default=0)
 
-    def save(self, *args, **kwargs):
-        model = self.__class__
-        if self.position is None:
-            try:
-                last = model.objects.order_by('-position')[0]
-                self.position = last.position + 1
-            except IndexError: self.position = 0
-        return super(Sortable, self).save(*args, **kwargs)
+    position = models.IntegerField(blank=True, help_text='Used for sorting')
 
     class Meta:
         abstract = True
         ordering = ('position',)
+
+    def save(self, *args, **kwargs):
+        """
+        autoincrement sorting
+        we do not care about concurrency on this project in this case
+        django is not able to use a second autoincrement field beside the primary key :(
+        """
+        if self.position is None:
+            try:
+                last = self.__class__.objects.order_by('-position')[0]
+                self.position = last.position + 1
+            except IndexError:
+                self.position = 0
+
+        return super(Sortable, self).save(*args, **kwargs)
