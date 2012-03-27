9dd08f350ebd71aff967c17221132719cea52601
Dominique Lederer <dominique.lederer@wunderweiss.com>
2012-03-27 16:55:05 +0200

    make sorting work with large lists

--- sortable/static/js/django-admin-sortable.js
+++ sortable/static/js/django-admin-sortable.js
@@ -17,10 +17,10 @@
             console.info("No sortable table found in the view, exiting");
             return;
         }
-    
+
         // Set this to the name of the column holding the position
         pos_field = 'position';
-    
+
         // Determine the column number of the position field
         pos_col = null;
         cols = $(table).find('tbody tr:first').children();
@@ -28,35 +28,35 @@
             inputs = $(cols[i]).find('input[name*=' + pos_field + ']');
             if (inputs.length > 0) {pos_col = i; break;}} // Found
         if (pos_col === null) return;                      // Not found
-            
+
         // Some visual enhancements
         header = $(table).find('thead tr').children()[pos_col];
         $(header).css('width', '1em');
         $(header).children('a').text('#');
-    
-        // Hide position field
+
+        // Hide position input field and show the value as read-only text
         $(table).find('tbody tr').each(function(index) {
             pos_td = $(this).children()[pos_col];
             input = $(pos_td).children('input').first();
             input.hide();
-    
+
             label = $('<strong>' + input.attr('value') + '</strong>');
             $(pos_td).append(label);
         });
-    
+
         // Determine sorted column and order
         sorted = $(table).find('thead th.sorted');
         sorted_col = $(table).find('thead th').index(sorted);
         sort_order = sorted.hasClass('descending') ? 'desc' : 'asc';
-    
+
         if (sorted_col != pos_col) {
             // Sorted column is not position column, bail out
             console.info("Sorted column is not %s, bailing out", pos_field);
             return;
         }
-    
+
         $(table).find('tbody tr').css('cursor', 'move');
-    
+
         // Make tbody > tr sortable
         $(table).find('tbody').sortable({
             items: 'tr',
@@ -64,21 +64,28 @@
             update: function(event, ui) {
                 item = ui.item;
                 items = $(this).find('tr').get();
-    
+
+                sort_values = $.map(items, function(item, i) {
+                    return parseInt($(item).children(':eq('+pos_col+')').find('input:first').val(), 10);
+                });
+
+                //make it work with paged lists
+                offset = Math.min.apply( Math, sort_values );
+
                 if (sort_order == 'desc') {
                     // Reverse order
                     items.reverse();
                 }
-    
+
                 $(items).each(function(index) {
                     pos_td = $(this).children()[pos_col];
                     input = $(pos_td).children('input').first();
                     label = $(pos_td).children('strong').first();
-    
-                    input.attr('value', index);
-                    label.text(index);
+
+                    input.attr('value', index+offset);
+                    label.text(index+offset);
                 });
-    
+
                 // Update row classes
                 $(this).find('tr').removeClass('row1').removeClass('row2');
                 $(this).find('tr:even').addClass('row1');
